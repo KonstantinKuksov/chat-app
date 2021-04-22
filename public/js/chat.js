43375a57c6ffdf2cf4b461af8784b41d7273bf60
KonstantinKuksov <kkuksov@gmail.com>
2021-04-22 15:08:09 +0300

    work commit

---
+++ public/js/chat.js
@@ -0,0 +1,25 @@
+const socket = io();
+
+socket.on('message', (message) => {
+  console.log(message);
+});
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+  socket.emit('sendMessage', message);
+  e.target.elements.message.value = '';
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(({ coords }) => {
+    socket.emit('sendLocation', {
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('New WebServer connection');
+
+  socket.emit('message', 'Welcome to chat!');
+  socket.broadcast.emit('message', 'A new user has joined!');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('sendLocation', (location) => {
+    io.emit(
+      'message',
+      `https://google.com/maps?q=${location.latitude},${location.longitude}`
+    );
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!');
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}`);
 });
