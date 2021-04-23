d3b65c3ec6548670a93fb31c35622f673cb69803
Konstantin Kuksov <kkuksov@gmail.com>
2021-04-23 21:57:19 +0300

    work commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,15 +14,17 @@ const locationTamplate = document.querySelector('#location-template').innerHTML;
 socket.on('message', (message) => {
   console.log(message);
   const html = Mustache.render(messegeTamplate, {
-    message,
+    message: message.text,
+    createdAt: moment(message.createdAt).format('HH:mm'),
   });
   $messages.insertAdjacentHTML('beforeend', html);
 });
 
-socket.on('locationMessage', (url) => {
-  console.log(url);
+socket.on('locationMessage', (message) => {
+  console.log(message);
   const html = Mustache.render(locationTamplate, {
-    url,
+    url: message.url,
+    createdAt: moment(message.createdAt).format('HH:mm'),
   });
   $messages.insertAdjacentHTML('beforeend', html);
 });
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const Filter = require('bad-words');
+const { groupEnd } = require('console');
 
 const PORT = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -16,8 +21,8 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
   console.log('New WebServer connection');
 
-  socket.emit('message', 'Welcome to chat!');
-  socket.broadcast.emit('message', 'A new user has joined!');
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit('message', generateMessage('A new user has joined!'));
 
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter();
@@ -26,20 +31,22 @@ io.on('connection', (socket) => {
       return callback('Profanity is not allowed');
     }
 
-    io.emit('message', message);
+    io.emit('message', generateMessage(message));
     callback();
   });
 
   socket.on('sendLocation', (location, callback) => {
     io.emit(
       'locationMessage',
-      `https://google.com/maps?q=${location.latitude},${location.longitude}`
+      generateLocationMessage(
+        `https://google.com/maps?q=${location.latitude},${location.longitude}`
+      )
     );
     callback();
   });
 
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left!');
+    io.emit('message', generateMessage('A user has left!'));
   });
 });
 
